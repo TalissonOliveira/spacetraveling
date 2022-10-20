1c1f205ec949aea4da2bf4cc4759ae497e26cac8
Talisson Oliveira <talissonlux@outlook.com>
2022-10-20 04:38:33 -0300

    feat: fetches post info with getStaticProps

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,13 +1,13 @@
-import { format } from 'date-fns'
+import Head from 'next/head';
 import { GetStaticPaths, GetStaticProps } from 'next';
-import Head from 'next/head'
-import { FiCalendar, FiClock, FiUser } from 'react-icons/fi'
-import Header from '../../components/Header'
-
+import { useRouter } from 'next/router';
+import { FiCalendar, FiClock, FiUser } from 'react-icons/fi';
+import { format } from 'date-fns';
+import { RichText } from 'prismic-dom';
 import { getPrismicClient } from '../../services/prismic';
+import Header from '../../components/Header';
 import ptBR from 'date-fns/locale/pt-BR';
 
-import commonStyles from '../../styles/common.module.scss';
 import styles from './post.module.scss';
 
 interface Post {
@@ -31,51 +31,64 @@ interface PostProps {
   post: Post;
 }
 
-export default function Post() {
+export default function Post({ post }: PostProps) {
+  const router = useRouter()
+
+  const estimatedReadingTime = post.data.content.reduce((acc, currentValue) => {
+    const numberOfWordsInTheTitle = currentValue.heading.split(' ').length
+    const numberOfWordsInTheBody = RichText.asText(currentValue.body).split(' ').length
+    const averageWordsPerMinute = 200
+
+    return acc + Math.ceil((numberOfWordsInTheTitle + numberOfWordsInTheBody) / averageWordsPerMinute)
+  }, 0)
+
+  if (router.isFallback) {
+    return (
+      <div>Carregando...</div>
+    )
+  }
+
   return (
     <>
       <Head>
-        <title>Post | SpaceTraveling</title>
+        <title>{post.data.title} | SpaceTraveling</title>
       </Head>
 
       <Header />
 
-      <img className={styles.banner} src="https://images.prismic.io/spacetraveling-challenge-reactjs/5966889c-1dea-4383-95d0-d6010ec0cc78_roman-synkevych-vXInUOv1n84-unsplash+2.png?auto=compress,format" alt="Banner" />
+      <img className={styles.banner} src={post.data.banner.url} alt="Banner do post" />
 
       <main className={styles.container}>
         <article className={styles.post}>
-          <h1>Criando um app CRA do zero</h1>
+         <h1>{post.data.title}</h1>
           <div className={styles.info}>
             <div>
               <FiCalendar size={20} />
               <time>
-                {format(
-                  new Date(),
-                  'dd MMM yyyy',
-                  {
-                    locale: ptBR,
-                  }
-                )}
+                {format(new Date(post.first_publication_date), 'dd MMM yyyy', {
+                  locale: ptBR,
+                })}
               </time>
             </div>
             <div>
               <FiUser size={20} />
-              <span>Talisson Oliveira</span>
+              <span>{post.data.author}</span>
             </div>
             <div>
               <FiClock size={20} />
-              <span>4 min</span>
+              <span>{estimatedReadingTime} min</span>
             </div>
           </div>
 
           <div className={styles.content}>
-            <h2>Introdução como criar um CRA</h2>
-            <p>
-              Nulla auctor sit amet quam vitae commodo. Sed risus justo, vulputate quis neque eget, dictum sodales sem. In eget felis finibus, mattis magna a, efficitur ex. Curabitur vitae justo consequat sapien gravida auctor a non risus. Sed malesuada mauris nec orci congue, interdum efficitur urna dignissim. Vivamus cursus elit sem, vel facilisis nulla pretium consectetur. Nunc congue.
-            </p>
-            <p>
-              Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aliquam consectetur massa nec metus condimentum, sed tincidunt enim tincidunt. Vestibulum fringilla risus sit amet massa suscipit eleifend. Duis eget metus cursus, suscipit ante ac, iaculis est. Donec accumsan enim sit amet lorem placerat, eu dapibus ex porta. Etiam a est in leo pulvinar auctor. Praesent sed vestibulum elit, consectetur egestas libero.
-            </p>
+            {post.data.content.map(postContent => (
+              <div key={postContent.heading}>
+                <h2>{postContent.heading}</h2>
+                <div
+                  dangerouslySetInnerHTML={{__html: RichText.asHtml(postContent.body)}}
+                />
+              </div>
+            ))}
           </div>
         </article>
       </main>
@@ -83,16 +96,30 @@ export default function Post() {
   )
 }
 
-// export const getStaticPaths = async () => {
-//   const prismic = getPrismicClient({});
-//   const posts = await prismic.getByType(TODO);
+export const getStaticPaths: GetStaticPaths = async () => {
+  const prismic = getPrismicClient({});
+  const posts = await prismic.getByType('posts');
+  const paths = posts.results.map(post => ({
+    params: {
+      slug: post.uid
+    }
+  }))
 
-//   // TODO
-// };
+  return {
+    paths,
+    fallback: true
+  }
+};
 
-// export const getStaticProps = async ({params }) => {
-//   const prismic = getPrismicClient({});
-//   const response = await prismic.getByUID(TODO);
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const { slug } = params
 
-//   // TODO
-// };
+  const prismic = getPrismicClient({});
+  const response = await prismic.getByUID('posts', String(slug));
+
+  return {
+    props: {
+      post: response
+    }
+  }
+};
